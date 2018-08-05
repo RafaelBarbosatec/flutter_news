7117c4e68e0e068b3734b078b1b01bb24bbfe6b9
Rafael Barbosa <rafael.barbosa@iterative.com.br>
2018-08-05 01:19:26 -0300

    simplificando o BottomNavigation

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -1,39 +1,5 @@
 import 'package:flutter/material.dart';
 
-class NavigationIconView {
-  NavigationIconView({
-    Widget icon,
-    String title,
-    Color color,
-    TickerProvider vsync,
-  }) : _icon = icon,
-        _color = color,
-        _title = title,
-        item = new BottomNavigationBarItem(
-          icon: icon,
-          title: new Text(title),
-          backgroundColor: color,
-        ),
-        controller = new AnimationController(
-          duration: kThemeAnimationDuration,
-          vsync: vsync,
-        ) {
-    _animation = new CurvedAnimation(
-      parent: controller,
-      curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
-    );
-  }
-
-  final Widget _icon;
-  final Color _color;
-  final String _title;
-  final BottomNavigationBarItem item;
-  final AnimationController controller;
-  CurvedAnimation _animation;
-
-}
-
-
 class BottomNavigation extends StatefulWidget {
 
   final callback;
@@ -45,89 +11,40 @@ class BottomNavigation extends StatefulWidget {
 
 }
 
-class _BottomNavigationState extends State<BottomNavigation>
-    with TickerProviderStateMixin {
+class _BottomNavigationState extends State<BottomNavigation> {
+
   int _currentIndex = 0;
   BottomNavigationBarType _type = BottomNavigationBarType.fixed;
-  List<NavigationIconView> _navigationViews;
-
-  @override
-  void initState() {
-    super.initState();
-    _navigationViews = <NavigationIconView>[
-      new NavigationIconView(
-        icon: const Icon(Icons.home),
-        title: 'Recentes',
-        color: Colors.blue,
-        vsync: this,
-      ),
-      new NavigationIconView(
-        icon: const Icon(Icons.list),
-        title: 'Notícias',
-        color: Colors.blue[800],
-        vsync: this,
-      ),
-      new NavigationIconView(
-        icon: const Icon(Icons.info),
-        title: 'Sobre',
-        color: Colors.blue,
-        vsync: this,
-      ),
-//      new NavigationIconView(
-//        icon: const Icon(Icons.laptop),
-//        title: 'Tecnologia',
-//        color: Colors.teal,
-//        vsync: this,
-//      ),
-//      new NavigationIconView(
-//        icon: const Icon(Icons.directions_bike),
-//        title: 'Esportes',
-//        color: Colors.green,
-//        vsync: this,
-//      ),
-//      new NavigationIconView(
-//        icon: const Icon(Icons.sentiment_very_satisfied),
-//        title: 'Entreterimento',
-//        color: Colors.red,
-//        vsync: this,
-//      )
-    ];
-
-    for (NavigationIconView view in _navigationViews)
-      view.controller.addListener(_rebuild);
-
-    _navigationViews[_currentIndex].controller.value = 1.0;
-  }
-
-  @override
-  void dispose() {
-    for (NavigationIconView view in _navigationViews)
-      view.controller.dispose();
-    super.dispose();
-  }
-
-  void _rebuild() {
-    setState(() {
-      // Rebuild in order to animate views.
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
 
     final BottomNavigationBar botNavBar = new BottomNavigationBar(
-      items: _navigationViews
-          .map((NavigationIconView navigationView) => navigationView.item)
-          .toList(),
+      items: [
+        new BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            title: Text('Recentes'),
+            backgroundColor: Colors.blue
+        ),
+        new BottomNavigationBarItem(
+            icon: const Icon(Icons.list),
+            title: Text('Notícias'),
+            backgroundColor: Colors.blue[800]
+        ),
+        new BottomNavigationBarItem(
+            icon: const Icon(Icons.info),
+            title: Text('Sobre'),
+            backgroundColor: Colors.blue
+        )
+      ],
       currentIndex: _currentIndex,
       type: _type,
       onTap: (int index) {
         setState(() {
-          _navigationViews[_currentIndex].controller.reverse();
-          _currentIndex = index;
-          _navigationViews[_currentIndex].controller.forward();
 
+          _currentIndex = index;
           widget.callback(index);
+
         });
       },
     );

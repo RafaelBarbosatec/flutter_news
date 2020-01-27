e76d336f3717afbe6ececc1e280e66654f1b7fef
Rafael  Barbosa - Sciensa <rafael.sciensa@MacBook-Pro-de-adminisrador.local>
2020-01-27 15:15:50 -0300

    atualiza dependencia e migra para androidX

---
+++ .dart_tool/flutter_build/b69fb62c6faba44d0103dfcf640cf696/armeabi-v7a/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /Users/rafael.sciensa/flutter/bin/cache/artifacts/engine/android-arm-release/darwin-x64/gen_snapshot
---
+++ .dart_tool/flutter_build/b69fb62c6faba44d0103dfcf640cf696/x86_64/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /Users/rafael.sciensa/flutter/bin/cache/artifacts/engine/android-x64-release/darwin-x64/gen_snapshot

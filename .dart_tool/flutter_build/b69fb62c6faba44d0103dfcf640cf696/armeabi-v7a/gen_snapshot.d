2c359b5cd17839bdbffd1c74e50fc44802bee6b8
Rafael <rafaelbarbosatec@gmail.com>
2020-02-16 18:54:28 -0300

    remove arquivos desnecessários

--- .dart_tool/flutter_build/b69fb62c6faba44d0103dfcf640cf696/armeabi-v7a/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /Users/rafael.sciensa/flutter/bin/cache/artifacts/engine/android-arm-release/darwin-x64/gen_snapshot
--- .dart_tool/flutter_build/b69fb62c6faba44d0103dfcf640cf696/x86_64/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /Users/rafael.sciensa/flutter/bin/cache/artifacts/engine/android-x64-release/darwin-x64/gen_snapshot
